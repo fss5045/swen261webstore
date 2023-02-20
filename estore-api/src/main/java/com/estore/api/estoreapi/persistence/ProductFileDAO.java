55f43d51240ae8e5e741c9ebda5d36b87aa1a0d9
Finn <fidrivenn@gmail.com>
2023-02-20 18:40:20 -0500

    working on productcontroller

---
+++ estore-api/src/main/java/com/estore/api/estoreapi/persistence/ProductFileDAO.java
@@ -0,0 +1,21 @@
+package com.estore.api.estoreapi.persistence;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.logging.Logger;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import com.estore.api.estoreapi.model.Product;;
+
+@Component
+public class ProductFileDAO {
+    private static final Logger LOG = Logger.getLogger(ProductFileDAO.class.getName());
+
+}
