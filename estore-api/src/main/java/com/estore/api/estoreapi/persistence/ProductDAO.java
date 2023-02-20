7ccdd76bb30e9db1eb0477f2bf0e40aafc0d7afc
Finn <fidrivenn@gmail.com>
2023-02-20 18:33:10 -0500

    productDAO

--- estore-api/src/main/java/com/estore/api/estoreapi/persistence/ProductDAO.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/persistence/ProductDAO.java
@@ -24,5 +24,54 @@ public interface ProductDAO {
      */
     Product[] findProducts(String containsText) throws IOException;
 
-    
+    /**
+     * Retrieves a {@linkplain Product product} with the given id
+     * 
+     * @param id The id of the {@link Product product} to get
+     * 
+     * @return a {@link Product producy} object with the matching id
+     * <br>
+     * null if no {@link Product product} with a matching id is found
+     * 
+     * @throws IOException if an issue with underlying storage
+     */
+    Product getProduct(int id) throws IOException;
+
+    /**
+     * Creates and saves a {@linkplain Product product}
+     * 
+     * @param hero {@linkplain Product product} object to be created and saved
+     * <br>
+     * The id of the product object is ignored and a new uniqe id is assigned
+     *
+     * @return new {@link Product product} if successful, false otherwise 
+     * 
+     * @throws IOException if an issue with underlying storage
+     */
+    Product  createProduct(Product product) throws IOException;
+
+    /**
+     * Updates and saves a {@linkplain Product product}
+     * 
+     * @param {@link Product product} object to be updated and saved
+     * 
+     * @return updated {@link Product product} if successful, null if
+     * {@link Prodcut prodcut} could not be found
+     * 
+     * @throws IOException if underlying storage cannot be accessed
+     */
+    Product updateProduct(Product product) throws IOException;
+
+    /**
+     * Deletes a {@linkplain Product product} with the given id
+     * 
+     * @param id The id of the {@link Product product}
+     * 
+     * @return true if the {@link Product product} was deleted
+     * <br>
+     * false if hero with the given id does not exist
+     * 
+     * @throws IOException if underlying storage cannot be accessed
+     */
+    boolean deleteProduct(int id) throws IOException;
 }
