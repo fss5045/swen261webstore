3e1091fd8f0433d951a135b56dfb4e92e530fb1b
Tashi Tseten <tot6836@g.rit.edu>
2023-03-01 19:40:57 -0500

    working on angular

--- estore-ui/angular-ui/src/app/app.module.ts
+++ estore-ui/angular-ui/src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProdcutsDetailComponent } from './products-detail/products-detail.component';
+import { Productsomponent } from './products/products.component';
+import { ProductsSearchComponent } from './products-search/products-search.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+
+    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+    // and returns simulated server responses.
+    // Remove it when a real server is ready to receive requests.
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    ProductsComponent,
+    ProductsDetailComponent,
+    MessagesComponent,
+    ProductsSearchComponent
+  ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
