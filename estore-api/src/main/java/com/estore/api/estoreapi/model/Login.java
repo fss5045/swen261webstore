d4d02bd6ae4bb6df88c84763efa23cfc7165af76
Justin Lin <jl6941@g.rit.edu>
2023-03-06 21:36:56 -0500

    modified login.java to incorporate User.java

--- estore-api/src/main/java/com/estore/api/estoreapi/model/Login.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/model/Login.java
@@ -1,10 +1,21 @@
 package com.estore.api.estoreapi.model;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import com.estore.api.estoreapi.model.User;
+
+
+
 import com.fasterxml.jackson.annotation.JsonProperty;
+
 
 public class Login {
     
-    @JsonProperty("username") private String username;
+    private static final Logger log = LoggerFactory.getLogger(Login.class);
+    
+    @JsonProperty("username")
+    private String username;
     private boolean isAdmin;
 
     public Login(@JsonProperty("username") String username){
@@ -15,5 +26,11 @@ public class Login {
         this.isAdmin = username.equals("admin");
         return isAdmin;
     }
-
+    
+    public ResponseEntity<User> addAdditionalUser(String username){
+        log.info("New user added: " + username);
+        // add logic to add new user to the database or user list
+        User newUser = new User(username);
+        return ResponseEntity.ok(newUser);
+    }
 }
