8441006c6bc8d024dfabf1adf20c7d727e309535
Finn <fss5045@rit.edu>
2023-03-22 16:03:28 -0400

    login

--- estore-api/src/main/java/com/estore/api/estoreapi/controller/LoginController.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/controller/LoginController.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -24,13 +25,15 @@ import com.estore.api.estoreapi.model.Enums.UserType;
 public class LoginController {
     private static final Logger LOG = Logger.getLogger(UserController.class.getName());
     private UserController userController;
+    private UserType currentUser;
 
     public LoginController(UserController userController){
         this.userController = userController;
+        this.currentUser = null;
     }
 
-    @PostMapping("")
-    public ResponseEntity<User> login(@RequestBody String username){
+    @PostMapping("/login/{username}")
+    public ResponseEntity<User> login(@PathVariable String username){
         LOG.info("POST /login " + username);
         User user = null;
         // create user if not already existing
@@ -43,12 +46,21 @@ public class LoginController {
         if(user.getUserType() == UserType.Admin){
             // login to admin page
             LOG.info("logging in as admin");
+            this.currentUser = UserType.Admin;
             return new ResponseEntity<User>(user, HttpStatus.OK);
         }
         else{
             // login to user page
             LOG.info("logging in as customer");
+            this.currentUser = UserType.Customer;
             return new ResponseEntity<User>(user, HttpStatus.OK);
         }
+    }
+
+    @PatchMapping("/logout")
+    public ResponseEntity<Boolean> logout(){
+        LOG.info("logging out");
+        this.currentUser = null;
+        return new ResponseEntity<Boolean>(true, HttpStatus.OK);
     }
 }
