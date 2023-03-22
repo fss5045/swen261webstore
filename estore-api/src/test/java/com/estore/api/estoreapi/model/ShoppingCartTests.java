53deea08c02fa0c51f9cc97d8bfad9cb6caea4b4
Justin Lin <jl6941@g.rit.edu>
2023-03-22 19:32:03 -0400

    shopping cart tests

--- estore-api/src/test/java/com/estore/api/estoreapi/model/ShoppingCartTests.java
+++ estore-api/src/test/java/com/estore/api/estoreapi/model/ShoppingCartTests.java
@@ -3,25 +3,50 @@ package com.estore.api.estoreapi.model;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import com.estore.api.estoreapi.persistence.ProductDAO;
 
 /**
- * tests the user.java file
+ * tests the shoppingcart file
  * <p>
  * 
  * @author jl6941@rit.edu
  */
-public class ShoppingCartTests {
+public class ShoppingCartTests{
     
     @Test
-    public void testAddItem(){
+    public void testConstructor(){
         // Setup
-        String expectedName = "test";
+        String expectedUsername = "test";
         int expectedId = 1;
-    }
-    @Test 
-    public void testRemoveItem(){
 
+        // Invoke
+        ShoppingCart result = new ShoppingCart(expectedUsername, expectedId);
+
+        // Analyze
+        assertEquals(expectedUsername, result.getUsername());
+        assertEquals(expectedId, result.getId());
     }
+
+
+    @Test
+    public void testAddItems(){
+        // Setup
+        ShoppingCart testUser = new ShoppingCart("testing", 1);
+        ArrayList<Object> testCart = new ArrayList<>();
+        testCart.add(productDAO.getProduct(id))
+    }
+
+        @Test
+        public void testRemoveItems(){
+            // Setup
+            ShoppingCart testUser = new ShoppingCart("testing", 1);
+            ArrayList<Object> testCart = new ArrayList<>();
+        }
 
     }
