6cdd4a2623d85c9c74a3c223756b247051e0130c
Justin Lin <jl6941@g.rit.edu>
2023-03-22 15:31:48 -0400

    shopping cartx3

--- estore-api/src/main/java/com/estore/api/estoreapi/model/ShoppingCart.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/model/ShoppingCart.java
@@ -10,7 +10,10 @@ import com.estore.api.estoreapi.controller.ProductController;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-// shopping cart needs to remove, add, edit quantity, checkout, 
+    /**
+     * Shopping cart functionality where the customer is able to modify the cart by checking out
+     * adding an item, removing and item. And admins should be able to edit a specific items attributes.
+     */
 public class ShoppingCart {
     private User user;
     private ProductDAO productDao;
@@ -19,13 +22,25 @@ public class ShoppingCart {
         this.user = user;
         this.productDao = productDao;
     }
-
+    /**
+     * Adds a {@linkplain Product product} to the shopping cart.
+     * 
+     * @param id The id of the {@link Product product} in the shopping cart. 
+     * 
+     * @return None
+     */
     public void addItem(int id) throws IOException{
         ArrayList<Product> cart  = user.getCart();
         cart.add(productDao.getProduct(id));
         user.setCart(cart);
     }
-    
+    /**
+     * Removes a {@linkplain Product product} to the shopping cart.
+     * 
+     * @param id The id of the {@link Product product} in the shopping cart. 
+     * 
+     * @return None
+     */
     public void removeItem(int id) throws IOException{
         ArrayList<Product> cart  = user.getCart();
         cart.remove(productDao.getProduct(id));
